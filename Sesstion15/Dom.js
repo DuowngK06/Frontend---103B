b055d4320c5162e6b001698e3b7b137cd9a22136
dh5147666 <dh5147666@gmail.com>
2026-03-05 22:25:39 +0700

    addcode

---
+++ Sesstion15/Dom.js
@@ -0,0 +1,18 @@
+console.log("Hello World");
+
+let todo = [
+    { id: 0, name: "Choi Game", isDone: true }
+     { id: 0, name: "Choi Game", isDone: true }
+ { id: 0, name: "Choi Game", isDone: true },
+];
+ 
+  // code funtion
+  const handleAddTodo = () =>[
+    for (
+        
+  
+
+
+
+    )
+  ]
